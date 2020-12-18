ce3a37076a208a3179bc15a5b7c8eab08b3170cf
Luis Zunino <zuninoluis01@gmail.com>
2020-12-17 22:52:26 -0300

    index.html
    se agregan estilos responsivos

--- ASSETS/JS/index.js
+++ ASSETS/JS/index.js
@@ -1,3 +1,4 @@
+//menu de hamburgesa
 // selector
 var menu = document.querySelector('.hamburger');
 
@@ -12,10 +13,32 @@ function toggleMenu (event) {
 menu.addEventListener('click', toggleMenu, false);
 
 //Solución con jQUery
-/*$(document).ready(function(){
+$(document).ready(function(){
 	$('.hamburger').click(function() {
 		$('.hamburger').toggleClass('is-active');
 		$('.menuresponsive').toggleClass('is-active');
 		return false;
 	});
-});*/
+});
+// fin de menu de hamburgesa
+
+// Index Responsivo
+$(function(){
+  $(".accordion-titulo").click(function(e){
+           
+        e.preventDefault();
+    
+        var contenido=$(this).next(".accordion-content");
+
+        if(contenido.css("display")=="none"){ //open		
+          contenido.slideDown(250);			
+          $(this).addClass("open");
+        }
+        else{ //close		
+          contenido.slideUp(250);
+          $(this).removeClass("open");	
+        }
+
+      });
+});
+// Fin de index responsivo 
